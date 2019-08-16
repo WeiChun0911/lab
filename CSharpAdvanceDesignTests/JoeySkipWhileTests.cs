91b58e50b31cc2cd9eb2b48ef1930b93198f1b9e
willy.liu89 <willy.liu@titansoft.com.sg>
2019-08-16 16:17:13 +0800

    add new function, and extract 條件到 呼叫端

--- CSharpAdvanceDesignTests/JoeySkipWhileTests.cs
+++ CSharpAdvanceDesignTests/JoeySkipWhileTests.cs
@@ -38,19 +38,66 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
-        private IEnumerable<Card> JoeySkipWhile(IEnumerable<Card> cards)
+
+        [Test]
+        public void skip_cards_when_point_less_than_5()
+        {
+            var cards = new List<Card>
+            {
+                new Card {Kind = CardKind.Normal, Point = 2},
+                new Card {Kind = CardKind.Normal, Point = 3},
+                new Card {Kind = CardKind.Normal, Point = 4},
+                new Card {Kind = CardKind.Separate},
+                new Card {Kind = CardKind.Normal, Point = 5},
+                new Card {Kind = CardKind.Normal, Point = 6},
+                new Card {Kind = CardKind.Separate},
+            };
+
+            var actual = JoeySkipWhilePointLessThan5(cards, card => card.Point < 5);
+
+            var expected = new List<Card>
+            {
+                new Card {Kind = CardKind.Normal, Point = 5},
+                new Card {Kind = CardKind.Normal, Point = 6},
+                new Card {Kind = CardKind.Separate},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<Card> JoeySkipWhilePointLessThan5(IEnumerable<Card> cards, Func<Card, bool> skip)
         {
             var enumerator = cards.GetEnumerator();
             bool matched = false;
             while (enumerator.MoveNext())
             {
                 var card = enumerator.Current;
-                if (card.Kind == CardKind.Separate) matched = true;
+                if (!skip(card))
+                {
+                    matched = true;
+                }
+
                 if (matched)
                 {
                     yield return card;
                 }
             }
+        }
+
+        private IEnumerable<Card> JoeySkipWhile(IEnumerable<Card> cards)
+        {
+            return JoeySkipWhilePointLessThan5(cards, card => card.Kind != CardKind.Separate);
+            //var enumerator = cards.GetEnumerator();
+            //bool matched = false;
+            //while (enumerator.MoveNext())
+            //{
+            //    var card = enumerator.Current;
+            //    if (card.Kind == CardKind.Separate) matched = true;
+            //    if (matched)
+            //    {
+            //        yield return card;
+            //    }
+            //}
         }
     }
 }
