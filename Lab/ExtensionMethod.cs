897b98aa2f1936b982aa334845c8c57bd7ab727f
willy.liu89 <willy.liu@titansoft.com.sg>
2019-08-16 14:31:23 +0800

    change to enumerator

--- Lab/ExtensionMethod.cs
+++ Lab/ExtensionMethod.cs
@@ -5,18 +5,26 @@ namespace Lab
 {
     public static class ExtensionMethod
     {
-        public static List<TSource> JoeyWhere<TSource>(this List<TSource> source, Func<TSource, bool> predicate)
+        public static IEnumerable<TSource> JoeyWhere<TSource>(this List<TSource> source, Func<TSource, bool> predicate)
         {
-            var result = new List<TSource>();
-            foreach (var item in source)
+            var enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
             {
+                var item = enumerator.Current;
                 if (predicate(item))
                 {
-                    result.Add(item);
+                    yield return item;
                 }
             }
+            //foreach (var item in source)
+            //{
+            //    if (predicate(item))
+            //    {
+            //        result.Add(item);
+            //    }
+            //}
 
-            return result;
+            //return result;
         }
 
         public static List<TSource> JoeyWhereWithIndex<TSource>(List<TSource> numbers, Func<TSource, int, bool> predicate)
@@ -31,6 +39,17 @@ namespace Lab
                 }
 
                 index++;
+            }
+
+            return list;
+        }
+
+        public static List<TResult> JoeySelect<TSource, TResult>(this List<TSource> employees, Func<TSource, TResult> selector)
+        {
+            var list = new List<TResult>();
+            foreach (var employee in employees)
+            {
+                list.Add(selector(employee));
             }
 
             return list;
@@ -57,16 +76,83 @@ namespace Lab
             }
             return list;
         }
-
-        public static List<TResult> JoeySelect<TSource,TResult>(this List<TSource> employees, Func<TSource, TResult> selector)
+        
+        public static IEnumerable<TResult> JoeySelect<TSource, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, int, TResult> selector)
         {
-            var list = new List<TResult>();
-            foreach (var employee in employees)
+            var result = new List<TResult>();
+            var index = 0;
+            foreach (var url in source)
             {
-                list.Add(selector(employee));
+                result.Add(selector(url, index));
+                index++;
             }
 
-            return list;
+            return result;
+        }
+
+        public static IEnumerable<TResult> JoeySelect<TSource, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, TResult> selector)
+        {
+            var enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var item = enumerator.Current;
+                yield return selector(item);
+            }
+
+            //var result = new List<TResult>();
+            //foreach (var item in source)
+            //{
+            //    result.Add(selector(item));
+            //}
+
+            //return result;
+        }
+
+        public static IEnumerable<TSource> JoeyWhere<TSource>(this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            var enumerator = source.GetEnumerator();
+            //var result = new List<TSource>();
+            while (enumerator.MoveNext())
+            {
+                var item = enumerator.Current;
+
+                if (predicate(item))
+                {
+                    //result.Add(item);
+                    yield return item;
+                }
+            }
+
+            //foreach (var item in source)
+            //{
+            //    if (predicate(item))
+            //    {
+            //        result.Add(item);
+            //    }
+            //}
+
+            //return result;
+        }
+
+        public static List<TSource> JoeyWhere<TSource>(this IEnumerable<TSource> numbers,
+            Func<TSource, int, bool> predicate)
+        {
+            var index = 0;
+            var result = new List<TSource>();
+            foreach (var number in numbers)
+            {
+                if (predicate(number, index))
+                {
+                    result.Add(number);
+                }
+
+                index++;
+            }
+
+            return result;
         }
     }
 }
