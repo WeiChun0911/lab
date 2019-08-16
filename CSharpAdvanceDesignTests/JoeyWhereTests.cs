3f7dfeb322df5d372167b029217cdd6622f11a61
willy.liu89 <willy.liu@titansoft.com.sg>
2019-08-16 10:47:07 +0800

    new test case

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -84,5 +84,20 @@ namespace CSharpAdvanceDesignTests
 
             expected.ToExpectedObject().ShouldMatch(actual); 
         }
+
+
+        [Test]
+        public void find_the_first_one_and_skip_second_one_and_take_other_positive_numbers()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, -5 };
+            var actual = JoeyWhereWithIndex(numbers);
+            var expected = new List<int> { 1, 3, 4 };
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private List<int> JoeyWhereWithIndex(List<int> numbers)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
