bd8a8946f986e6efe23370ce4d3d682f2ed99d8c
willy.liu89 <willy.liu@titansoft.com.sg>
2019-08-17 11:14:39 +0800

    use enumerator to implement FirstOrDefault

--- CSharpAdvanceDesignTests/JoeyFirstOrDefaultTests.cs
+++ CSharpAdvanceDesignTests/JoeyFirstOrDefaultTests.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
     public class JoeyFirstOrDefaultTests
     {
         [Test]
@@ -22,7 +21,13 @@ namespace CSharpAdvanceDesignTests
 
         private Employee JoeyFirstOrDefault(IEnumerable<Employee> employees)
         {
-            throw new NotImplementedException();
+            var enumerator = employees.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                return enumerator.Current;
+            }
+
+            return null;
         }
     }
 }
