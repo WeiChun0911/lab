5c16bea99b4e97c81fbc84adb6eeabe052d03274
willy.liu89 <willy.liu@titansoft.com.sg>
2019-08-17 10:41:39 +0800

    use enumerator to implement first

--- CSharpAdvanceDesignTests/JoeyFirstTests.cs
+++ CSharpAdvanceDesignTests/JoeyFirstTests.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyFirstTests
     {
         [Test]
@@ -27,7 +26,13 @@ namespace CSharpAdvanceDesignTests
 
         private Girl JoeyFirst(IEnumerable<Girl> girls)
         {
-            throw new System.NotImplementedException();
+            var enumerator = girls.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                return enumerator.Current;
+            }
+
+            return null;
         }
     }
 }
