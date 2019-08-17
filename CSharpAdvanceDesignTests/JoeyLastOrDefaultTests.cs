89de908106a395dae745faaed2f952ae21aebec9
willy.liu89 <willy.liu@titansoft.com.sg>
2019-08-17 11:56:05 +0800

    add new test, last or default with condition

--- CSharpAdvanceDesignTests/JoeyLastOrDefaultTests.cs
+++ CSharpAdvanceDesignTests/JoeyLastOrDefaultTests.cs
@@ -1,12 +1,13 @@
-﻿using Lab.Entities;
+﻿using System;
+using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
+using ExpectedObjects;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
     public class JoeyLastOrDefaultTests
     {
         [Test]
@@ -15,6 +16,38 @@ namespace CSharpAdvanceDesignTests
             var employees = new List<Employee>();
             var actual = JoeyLastOrDefault(employees);
             Assert.IsNull(actual);
+        }
+
+
+        [Test]
+        public void get_last_employee_who_is_manager()
+        {
+            var employees = new List<Employee>()
+            {
+                new Employee() {FirstName = "Joey", Role = Role.Manager},
+                new Employee() {FirstName = "David", Role = Role.Designer},
+                new Employee() {FirstName = "Tom", Role = Role.Manager},
+                new Employee() {FirstName = "May", Role = Role.Engineer},
+            };
+            var actual = JoeyLastOrDefaultWithCondition(employees, employee => employee.Role == Role.Manager);
+            new Employee() { FirstName = "Tom", Role = Role.Manager }.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private TSource JoeyLastOrDefaultWithCondition<TSource>(IEnumerable<TSource> employees, Func<TSource, bool> predicate)
+        {
+            var enumerator = employees.GetEnumerator();
+            var result = default(TSource);
+
+            while (enumerator.MoveNext())
+            {
+                var employee = enumerator.Current;
+                if (predicate(employee))
+                {
+                    result = employee;
+                }
+            }
+
+            return result;
         }
 
         private Employee JoeyLastOrDefault(IEnumerable<Employee> employees)
