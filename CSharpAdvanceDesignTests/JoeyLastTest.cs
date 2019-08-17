1d96783223e7078982c72fe717f49642a2d3b856
willy.liu89 <willy.liu@titansoft.com.sg>
2019-08-17 11:22:25 +0800

    add new test

---
+++ CSharpAdvanceDesignTests/JoeyLastTest.cs
@@ -0,0 +1,23 @@
+﻿using ExpectedObjects;
+using NUnit.Framework;
+
+namespace CSharpAdvanceDesignTests
+{
+    [TestFixture]
+    public class JoeyLastTest
+    {
+
+        [Test]
+        public void get_last_number()
+        {
+            var numbers = new[] { 1, 2, 3, 4 };
+            var actual = JoeyLast(numbers);
+            4.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private int JoeyLast(int[] numbers)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
