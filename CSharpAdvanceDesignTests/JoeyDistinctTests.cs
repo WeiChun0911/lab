3a30dd90e4c7da62bd0b57d9c226317f25eec760
willy.liu89 <willy.liu@titansoft.com.sg>
2019-08-18 14:36:03 +0800

    Implement IEqualityComparer, 來定義相等的條件還有 hash 的方法 傳進 HashSet 來做 distinct

--- CSharpAdvanceDesignTests/JoeyDistinctTests.cs
+++ CSharpAdvanceDesignTests/JoeyDistinctTests.cs
@@ -1,7 +1,9 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
+using Lab.Entities;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -17,6 +19,46 @@ namespace CSharpAdvanceDesignTests
             var expected = new[] { 91, 3, -1 };
 
             expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+
+        [Test]
+        public void distinct_employees()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+            };
+
+            var actual = JoeyDistinct(employees, new EmployeeCompareCondition());
+
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<Employee> JoeyDistinct(IEnumerable<Employee> employees, EmployeeCompareCondition employeeCompareCondition)
+        {
+            //var enumerator = employees.GetEnumerator();
+            //var result = new HashSet<Employee>(employeeCompareCondition);
+            //while (enumerator.MoveNext())
+            //{
+            //    var current = enumerator.Current;
+
+            //    if (result.Add(current))
+            //    {
+            //        yield return current;
+            //    }
+            //}
+            return new HashSet<Employee>(employees,employeeCompareCondition);
         }
 
         private IEnumerable<int> Distinct(IEnumerable<int> numbers)
@@ -38,6 +80,20 @@ namespace CSharpAdvanceDesignTests
 
             //return alreadyExistNumber;
             return new HashSet<int>(numbers);
+        }
+    }
+
+    internal class EmployeeCompareCondition : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee x, Employee y)
+        {
+            return x.FirstName == y.FirstName && x.LastName == y.LastName;
+        }
+
+        public int GetHashCode(Employee obj)
+        {
+            //return new Tuple(obj.FirstName,obj.LastName)
+            return new {obj.FirstName,obj.LastName}.GetHashCode();
         }
     }
 }
