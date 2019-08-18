a01c3aa73ec91a7edaf15c0b917ce245f84bb627
willy.liu89 <willy.liu@titansoft.com.sg>
2019-08-18 15:07:55 +0800

    use HashSet to implement Union

--- CSharpAdvanceDesignTests/JoeyUnionTests.cs
+++ CSharpAdvanceDesignTests/JoeyUnionTests.cs
@@ -5,13 +5,12 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyUnionTests
     {
         [Test]
         public void union_numbers()
         {
-            var first = new[] { 1, 3, 5 };
+            var first = new[] { 1, 3, 5, 1 };
             var second = new[] { 5, 3, 7 };
 
             var actual = JoeyUnion(first, second);
@@ -22,7 +21,25 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<int> JoeyUnion(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var temp = new HashSet<int>();
+            var enumerator = first.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (temp.Add(current))
+                {
+                    yield return current;
+                }
+            }
+            enumerator = second.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (temp.Add(current))
+                {
+                    yield return current;
+                }
+            }
         }
     }
 }
