c67d97f74d9d5fc9659cb52740c1253f7ec40b1e
willy.liu89 <willy.liu@titansoft.com.sg>
2019-08-18 15:31:00 +0800

    use hashSet to implement Except

--- CSharpAdvanceDesignTests/JoeyExceptTests.cs
+++ CSharpAdvanceDesignTests/JoeyExceptTests.cs
@@ -5,14 +5,13 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyExceptTests
     {
         [Test]
         public void except_numbers()
         {
-            var first = new[] { 1, 3, 5, 7 };
-            var second = new[] { 7, 1, 4 };
+            var first = new[] { 1, 3, 5, 7, 3};
+            var second = new[] { 7, 1, 4, 1};
 
             var actual = JoeyExcept(first, second);
             var expected = new[] { 3, 5 };
@@ -22,7 +21,38 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<int> JoeyExcept(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            //可惡 不行 不知道壞在哪
+            //var firstEnumerator = first.GetEnumerator();
+            //while (firstEnumerator.MoveNext())
+            //{
+            //    var firstCurrent = firstEnumerator.Current;
+            //    var shouldReturn = true;
+            //    var secondEnumerator = second.GetEnumerator();
+            //    while (secondEnumerator.MoveNext())
+            //    {
+            //        var secondCurrent = secondEnumerator.Current;
+            //        if (firstCurrent == secondCurrent)
+            //        {
+            //            shouldReturn = false;
+            //        }
+            //    }
+
+            //    if (shouldReturn)
+            //    {
+            //        yield return firstCurrent;
+            //    }
+            //}
+
+            var hashSet = new HashSet<int>(second);
+            var enumerator = first.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (hashSet.Add(current))
+                {
+                    yield return current;
+                }
+            }
         }
     }
 }
